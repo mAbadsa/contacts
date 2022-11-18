5a6a9432b2dea434b376a9a054b468320da0ae27
mAbadsa <moh22389@gmail.com>
2022-11-18 14:08:09 +0200

    add input change value testing

--- src/components/Search/__test__/Search.test.tsx
+++ src/components/Search/__test__/Search.test.tsx
@@ -1,12 +1,21 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Search from "../Search";
-// import {fn} from "jest";
+
+const searchFnMock = jest.fn();
 
 describe("Search", () => {
   test("Search component should have input element with placeholder", () => {
-    render(<Search search={() => {}} />);
-    const buttonElm = screen.getByPlaceholderText("Search contacts");
-    expect(buttonElm).toBeInTheDocument();
+    render(<Search search={searchFnMock} />);
+    const inputElm = screen.getByPlaceholderText("Search contacts");
+    expect(inputElm).toBeInTheDocument();
+  });
+
+  test("Search function", () => {
+    render(<Search search={searchFnMock} />);
+    const inputElm: HTMLInputElement =
+      screen.getByPlaceholderText("Search contacts");
+    fireEvent.change(inputElm, { target: { value: "Muhammad" } });
+    expect(inputElm.value).toBe("Muhammad");
   });
 });
 
